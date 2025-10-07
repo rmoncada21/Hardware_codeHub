d1da18845fc880f0f6c7b72546060a6618fa9c22
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2025-10-06 20:38:35 -0600

    SV_design - User_defined_data. Add packages.sv & testbench_enum.sv. Files to test enum types

---
+++ SystemVerilog/For_design/User_defined_data/testbench_enum.sv
@@ -0,0 +1,28 @@
+`include "packages.sv"
+
+`define SHOW_COLOR(data) $display("Valor: %d, name=%s", data, data.name);
+
+module testbench;
+    
+    // typedef enum int {RED, GREEN, BLUE} RGB_t;
+    // typedef enum int {COVERAGE, CHECKS, FATAL, DEFAULT[2]} FLAGS_ENABLE_T;
+    RGB_T rgbA;
+    FLAGS_T flags;
+
+    initial begin: testbench_main
+        // test RGB_T    
+        $display("TEST RGB_T enum");
+        rgbA = RED; `SHOW_COLOR(rgbA)
+        rgbA = GREEN; `SHOW_COLOR(rgbA)
+
+        // test FLAGS_ENABLE_T
+        // FLAGS_ENABLE_T flags;
+        $display("TEST FLAGS_ENABLE_T enum");
+        flags = COVERAGE; `SHOW_COLOR(flags)
+        flags = CHECKS;   `SHOW_COLOR(flags)
+        flags = FATAL;   `SHOW_COLOR(flags)
+        flags = DEFAULT0; `SHOW_COLOR(flags)
+        flags = DEFAULT1; `SHOW_COLOR(flags)
+        $finish;
+    end
+endmodule
