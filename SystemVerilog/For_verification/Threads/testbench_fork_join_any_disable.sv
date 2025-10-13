33ec627920150eb1fbef3f2bebf80b5173013b6c
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2025-10-12 23:06:12 -0600

    SV_verification - Thread-fork_join_any. Add testbench_fork_join_any.sv testbench_fork_join_any_disable.sv testbench_fork_join_any_wait.sv output/testbench_fork_join_any_sim.txt output/testbench_fork_join_any_disable_sim.txt output/testbench_fork_join_any_wait_sim.txt. Files to test fork join_any process
    	nuevos archivos: SystemVerilog/For_verification/Threads/output/testbench_fork_join_any_disable_sim.txt
    	nuevos archivos: SystemVerilog/For_verification/Threads/output/testbench_fork_join_any_sim.txt
    	nuevos archivos: SystemVerilog/For_verification/Threads/output/testbench_fork_join_any_wait_sim.txt
    	nuevos archivos: SystemVerilog/For_verification/Threads/testbench_fork_join_any.sv
    	nuevos archivos: SystemVerilog/For_verification/Threads/testbench_fork_join_any_disable.sv
    	nuevos archivos: SystemVerilog/For_verification/Threads/testbench_fork_join_any_wait.sv

---
+++ SystemVerilog/For_verification/Threads/testbench_fork_join_any_disable.sv
@@ -0,0 +1,49 @@
+/*
+    En fork-join_any, todos los procesos se inician simultáneamente 
+    y la unión esperará a que se completen uno los procesos para 
+    seguir con el programa.
+*/
+
+module main;
+    initial begin
+        fork: threads_any
+            begin: ProcesoA
+                $display("[Process A]: inicio:%0t", $time);
+                #10;
+                $display("[Process A]: fin:%0t", $time);
+            end
+
+            begin: ProcesoB
+                $display("[Process B]: inicio:%0t", $time);
+                #20;
+                $display("[Process B]: fin:%0t", $time);
+            end
+
+            begin: ProcesoC
+                $display("[Process C]: inicio:%0t", $time);
+                #30;
+                $display("[Process C]: fin:%0t", $time);
+            end
+        join_any: threads_any
+        disable fork;
+        $display("fork-join completo tiempo:%0t", $time);
+    end
+endmodule
+
+/* Salida del programa SIN disable fork
+[Process A]: inicio:0
+[Process B]: inicio:0
+[Process C]: inicio:0
+[Process A]: fin:10
+fork-join completo tiempo:10
+[Process B]: fin:20
+[Process C]: fin:30
+*/
+
+/* Salida del programa CON disable fork
+[Process A]: inicio:0
+[Process B]: inicio:0
+[Process C]: inicio:0
+[Process A]: fin:10
+fork-join completo tiempo:10
+*/
